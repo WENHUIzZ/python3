b039ef8fdfb9d28241159cb7f30f046d7dc3b11e
wenhui.yang <>
2021-03-04 10:19:45 +0800

    找出list里的重复值

---
+++ list_count.py
@@ -0,0 +1,16 @@
+"""输出list里重复的值"""
+
+
+def findRepeatNumber(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+
+
+    for i in set(nums):
+        if nums.count(i) > 1:
+            print(i)
+
+
+findRepeatNumber([12, 13, 14, 15, 12, 13])
